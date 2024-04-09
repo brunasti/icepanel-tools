0b90f45f48fe0db53cf2bcdf493a86018f481e0c
brunasti <paolo@brunasti.it>
2024-04-09 11:17:29 +0200

    Regenerates diagrams for BA with fixed IcePanel json

--- src/test/java/it/brunasti/icepanel/tools/TestConstants.java
+++ src/test/java/it/brunasti/icepanel/tools/TestConstants.java
@@ -21,7 +21,7 @@ public interface TestConstants {
 
 //  String srcDirectory = "./src/";
   String icePanelJSONFile = testResourcesDirectory + "icepanel-structured.json";
-  String icePanelJSONFileCompact_02 = testResourcesDirectory + "icepanel-plain-02.json";
+  String icePanelJSONFileCompact_02 = testResourcesDirectory + "icepanel-plain.json";
 //  String testJavaSrcDirectory = srcDirectory + "test/java";
 
 //  String classesDirectory = "./target/classes";
